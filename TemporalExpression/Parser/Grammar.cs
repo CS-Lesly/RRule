6c532022f877efd4c1971dc1dd727c8c9a0ccf72
CS-Lesly <L.Weyts@chipsoft.com>
2026-05-07 10:22:18 +0000

    Initial version of the Grammar helperclasses

---
+++ TemporalExpression/Parser/Grammar.cs
@@ -0,0 +1,49 @@
+namespace TemporalExpression.Parser;
+
+public abstract class Expression
+{
+    public static implicit operator Expression(char value) => new Terminal(value.ToString());
+    public static implicit operator Expression(string value) => new Terminal(value);
+
+    public static Expression operator +(Expression first, Expression second) => new Sequence(first, second);
+    public static Expression operator |(Expression first, Expression second) => new Choice(first, second);
+
+    public Expression Optional() => new Optional(this);
+
+    public Expression ZeroOrMore() => new Repeat(this, minimum: 0);
+    public Expression OneOrMore()  => new Repeat(this, minimum: 1);
+}
+
+public class Terminal(string value) : Expression
+{
+    public string Value { get; init; } = value;
+
+    public const string COLON = ":";
+    public const string SEMICOLON = ";";
+    public const string COMMA = ",";
+    public const string EQUALS = "=";
+    public const string NEWLINE = "\r\n";
+}
+
+public class Sequence(params Expression[] expressions) : Expression
+{
+    public Expression[] Expressions { get; init; } = expressions;
+}
+
+public class Choice(params Expression[] options) : Expression
+{
+    public Expression[] Options { get; init; } = options;
+}
+
+public class Optional(Expression expression) : Expression
+{
+    public Expression Expression { get; init; } = expression;
+}
+
+public class Repeat(Expression expression, int minimum = 0, int? maximum = null) : Expression
+{
+    public Expression Expression { get; init; } = expression;
+
+    public int Minimum { get; init; } = minimum;
+    public int? Maximum { get; init; } = maximum;
+}

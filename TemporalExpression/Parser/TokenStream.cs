4f33c3f6115351cf686b7a9565e5013993010012
CS-Lesly <L.Weyts@chipsoft.com>
2026-05-11 11:26:31 +0000

    MatchAndConsume for Terminals

--- TemporalExpression/Parser/TokenStream.cs
+++ TemporalExpression/Parser/TokenStream.cs
@@ -47,6 +47,8 @@ public ref struct TokenStream
         ? 3 // \r\n followed by space or tab is a fold, so skip all three characters
         : 0;
 
+    public bool MatchAndConsume(Terminal expected) => MatchAndConsume(expected.Value);
+
     public bool MatchAndConsume(string expected)
     {
         int temporaryPosition = Position;

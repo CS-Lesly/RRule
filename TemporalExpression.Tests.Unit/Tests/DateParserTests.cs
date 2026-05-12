bf1ef73d7b26809737714a3cd15fb57efc254807
CS-Lesly <L.Weyts@chipsoft.com>
2026-05-12 08:01:12 +0000

    Added unittests for DateOnly- and DateTimeValue

---
+++ TemporalExpression.Tests.Unit/Tests/DateParserTests.cs
@@ -0,0 +1,115 @@
+﻿using TemporalExpression.Parser;
+using TemporalExpression.Parser.Tokens.Values;
+
+namespace TemporalExpression.Tests;
+
+[TestFixture]
+public class DateParserTests
+{
+    [Test]
+    [TestCaseSource(nameof(GetDateOnlyTestSets))]
+    public void TestDateOnlyParsing(string input, DateOnlyResult expected)
+    {
+        var inputStream = new TokenStream(input);
+        var parseResult = new DateOnlyValue().Parse(ref inputStream);
+
+        if (expected.IsSuccess)
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(parseResult.Error, Is.Null);
+                Assert.That(parseResult.Value, Is.Not.Null);
+            }
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(parseResult.Value, Is.InstanceOf<DateOnlyValue>());
+                Assert.That(((DateOnlyValue)parseResult.Value!).Value, Is.Not.Null);
+            }
+
+            Assert.That(((DateOnlyValue)parseResult.Value!).Value!.Value, Is.EqualTo(expected.DateOnly));
+        }
+        else
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(parseResult.Error, Is.Not.Null);
+                Assert.That(parseResult.Value, Is.Null);
+            }
+        }
+    }
+
+    public record struct DateOnlyResult(bool IsSuccess, DateOnly DateOnly = default);
+
+    private static IEnumerable<TestCaseData> GetDateOnlyTestSets()
+    {
+        yield return new TestCaseData("20260101", new DateOnlyResult(IsSuccess: true, new DateOnly(2026, 1, 1)))
+            .SetName("Valid date")
+            .SetCategory("Valid data");
+
+        yield return new TestCaseData("260101", new DateOnlyResult(IsSuccess: false))
+            .SetName("Invalid date: missing century digits")
+            .SetCategory("Invalid data");
+        yield return new TestCaseData("20269901", new DateOnlyResult(IsSuccess: false))
+            .SetName("Invalid date: date part includes invalid month")
+            .SetCategory("Invalid data");
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetDateTimeTestSets))]
+    public void TestDateTimeParsing(string input, DateTimeResult expected)
+    {
+        var inputStream = new TokenStream(input);
+        var parseResult = new DateTimeValue().Parse(ref inputStream);
+
+        if (expected.IsSuccess)
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(parseResult.Error, Is.Null);
+                Assert.That(parseResult.Value, Is.Not.Null);
+            }
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(parseResult.Value, Is.InstanceOf<DateTimeValue>());
+                Assert.That(((DateTimeValue)parseResult.Value!).Value, Is.Not.Null);
+            }
+
+            Assert.That(((DateTimeValue)parseResult.Value!).Value!.Value, Is.EqualTo(expected.DateTime));
+        }
+        else
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(parseResult.Error, Is.Not.Null);
+                Assert.That(parseResult.Value, Is.Null);
+            }
+        }
+    }
+
+    public record struct DateTimeResult(bool IsSuccess, DateTime DateTime = default);
+
+    private static IEnumerable<TestCaseData> GetDateTimeTestSets()
+    {
+        yield return new TestCaseData("20260101T0000", new DateTimeResult(IsSuccess: true, new DateTime(2026, 1, 1)))
+            .SetName("Valid date-time with only years, months and days")
+            .SetCategory("Valid data");
+        yield return new TestCaseData("20260101T1234", new DateTimeResult(IsSuccess: true, new DateTime(2026, 1, 1, 12, 34, 0)))
+            .SetName("Valid date-time (with time part but not including seconds)")
+            .SetCategory("Valid data");
+
+        yield return new TestCaseData("260101T1234", new DateTimeResult(IsSuccess: false))
+            .SetName("Invalid date-time: missing century digits")
+            .SetCategory("Invalid data");
+        yield return new TestCaseData("20260101T123456", new DateTimeResult(IsSuccess: false))
+            .SetName("Invalid date-time: includes seconds")
+            .SetCategory("Invalid data");
+        yield return new TestCaseData("202601011234", new DateTimeResult(IsSuccess: false))
+            .SetName("Invalid date-time with missing T separator between date and time components")
+            .SetCategory("Invalid data");
+        yield return new TestCaseData("20269901T1234", new DateTimeResult(IsSuccess: false))
+            .SetName("Invalid date-time: date part includes invalid month")
+            .SetCategory("Invalid data");
+    }
+}
